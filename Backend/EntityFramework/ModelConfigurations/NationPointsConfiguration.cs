35c1a45610932ac0b407a3c15fa2f7183a3180f4
Thingholm <rasmusthingholm@gmail.com>
2024-06-28 18:04:39 +0200

    Dependency fix

--- Backend/EntityFramework/ModelConfigurations/NationPointsConfiguration.cs
+++ Backend/EntityFramework/ModelConfigurations/NationPointsConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EntityFramework.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
