cae03b190fa2995a19a1d5f7f005332c8c0053f0
Thingholm <rasmusthingholm@gmail.com>
2024-08-04 17:42:03 +0200

    previous placement model from db

---
+++ Backend/EntityFramework/Models/PreviousPlacement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Models
+{
+    [Table("previous_placements")]
+    public class PreviousPlacement : IModel
+    {
+        [Column("id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Column("rider_id")]
+        [ForeignKey(nameof(Rider))]
+        public int RiderId { get; set; }
+        public Rider Rider { get; set; }
+        [Column("result_id")]
+        [ForeignKey(nameof(Result))]
+        public int ResultId { get; set; }
+        public Result Result { get; set; }
+        [Column("prev_points")]
+        public int PrevPoints { get; set; }
+        [Column("prev_placement")]
+        public int PrevPlacement {  get; set; }
+        [Column("new_points")]
+        public int NewPoints { get; set; }
+        [Column("new_placement")]
+        public int NewPlacement { get; set; }
+
+    }
+}
